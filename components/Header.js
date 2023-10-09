300c1858db257fabd06d535b6f5f85a2e35af55a
lucresNova9393 <61306784+lucresNova9393@users.noreply.github.com>
2023-10-09 19:02:47 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ import Link from 'next/link';
 import Socials from '@/components/Socials';
 
 const Header = () => {
-  return <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+  return <header className="absolute z-30 w-full flex items-center px-4 xl:px-0 xl:h-[90px]">
     
 
     <div className="container mx-auto">
