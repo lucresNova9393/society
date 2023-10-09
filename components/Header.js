1c4a74e1585fdbcef7593443497d62a7e2610f43
lucresNova9393 <61306784+lucresNova9393@users.noreply.github.com>
2023-10-09 18:48:59 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,10 @@ import Socials from '@/components/Socials';
 
 const Header = () => {
   return <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+    
+            <p class="mb-0 text-sm text-center text-white-900 dark:text-white-900 sm:mb-0">
+  9 Yola Street,Area 7, Garki Abuja-Nigeria.+2347034797022.
+  </p>
     <div className="container mx-auto">
       <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 
       py-8">
@@ -21,9 +25,6 @@ const Header = () => {
           alt=""
           priority={true} 
           />
-           <div className="text-sm"> <p class="mb-0 text-sm text-center text-white-900 dark:text-white-900 sm:mb-0">
-  9 Yola Street,Area 7, Garki Abuja-Nigeria.+2347034797022.
-  </p></div>
         </Link>
       
         {/* Socials */}
