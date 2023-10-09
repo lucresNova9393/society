c7f81c2f7e7e2593a572187c5439af7e226c7746
lucresNova9393 <61306784+lucresNova9393@users.noreply.github.com>
2023-10-09 18:21:36 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -20,7 +20,7 @@ const Layout = ({children}) => {
   return (
   <div className={'page bg-site text-white bg-cover bg-no-repeat ${sora-variable} font-sora relative'}>
     {/*<Footer/>*/}
-    <TopLeftImg/>
+  {/*<TopLeftImg/>*/}
     <Nav/>
     <Header/>
     {children}
